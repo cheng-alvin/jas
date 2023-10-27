320156d10365e6a7df864b6fc1d2de0b19861412
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-10-28 09:15:49 +1100

    Added documentation

--- src/parse.h
+++ src/parse.h
@@ -5,4 +5,21 @@ typedef struct {
   char *arg3;
 } instruction_t;
 
+/**
+ * @author cheng-alvin
+ * @since v0.0.1
+ *
+ * Parses assembly instructions into an array of `instruction_t` structs.
+ * Assembly instructions are made of 4 parts: the instruction, and up to 3
+ * operands or otherwise known as arguments. (In a traditional programming
+ * sense)
+ *
+ * @example [instruction] [arg1], [arg2], [arg3]
+ *
+ * @note The instruction is space-separated from the arguments, however, the
+ * arguments are comma *AND* space separated from each other.
+ *
+ * @param buffer The stringified assembly instructions to parse.
+ */
+
 instruction_t **parse(char *buffer);
