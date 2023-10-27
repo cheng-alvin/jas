7d6967302ac7b3bf0283d6b0cf4f7e517ed37134
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-10-28 00:00:14 +1100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+#include "parse.h"
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Hello World!\n");
+  char in[] = "mov rax, 0x60 \nmov rax, 0x60";
+  char **lines = parse(in);
   return 0;
 }
