ea2fd323c26bacc5d41f0545ca6ac079e0387849
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-02 21:22:55 +1100

    Added endian functions

---
+++ include/endian.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+/**
+ * Converts 16, 32 and 64 bit unsigned integers to little endian
+ * values respectively. This is used for the code generation process
+ * and is portable to another codebase.
+ *
+ * @param value A 16, 32 or 64 bit unsigned integer in big endian to
+ * be converted into little endian number.
+ *
+ * @returns The little endian result with the same size as the parameter.
+ */
+
+uint16_t little_endian16(uint16_t value);
+uint32_t little_endian32(uint32_t value);
+uint64_t little_endian64(uint64_t value);
