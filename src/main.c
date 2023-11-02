5c7b7e72a7dfb8e2c40d1731258989f24d626d87
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-02 23:22:41 +1100

    WIP: main function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
+#include "endian.h"
+#include "instructions.h"
 #include "parse.h"
+#include "registers.h"
+#include "write.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  char in[] = "mov rax, 0x60 \nmov rax, 0x60";
-  instruction_t **lines = parse(in);
+  uint8_t *buf = (uint8_t *)malloc(1);
+  int len = 0;
+  write8(buf, 0xFF, &len);
+  write16(buf, 0xAD98, &len);
+  write16(buf, 0x1235, &len);
 
-  free(lines);
+  for (int i = 0; i < 10; i++) {
+    printf("%02X\n ", buf[i]);
+  }
 
   return 0;
 }
