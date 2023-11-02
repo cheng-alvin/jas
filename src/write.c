068d8f091c45eb7987031b3ae0a871d268bfe7bb
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-02 23:22:44 +1100

    Update write.c

--- src/write.c
+++ src/write.c
@@ -1,22 +1,26 @@
 #include "write.h"
+#include "endian.h"
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-uint8_t *write8(uint8_t *buffer, uint8_t value) {
-  const int len = *buffer / sizeof(uint8_t);
-  buffer = realloc(buffer, len + sizeof(uint8_t));
+uint8_t *write8(uint8_t *buffer, uint8_t value, int *len) {
+  buffer = realloc(buffer, (*len) + sizeof(uint8_t));
 
-  buffer[len] = value;
+  buffer[(*len)] = value;
+  (*len) += sizeof(uint8_t);
 
   return buffer;
 }
 
-uint8_t *write16(uint8_t *buffer, uint16_t value) {
-  const int len = *buffer / sizeof(uint16_t);
-  buffer = realloc(buffer, len + sizeof(uint16_t));
-
+uint8_t *write16(uint8_t *buffer, uint16_t value, int *len) {
+  buffer = realloc(buffer, (*len) + sizeof(uint16_t));
   value = little_endian16(value);
-  buffer[len] = value;
+
+  buffer[(*len)] = (value >> 8) & 0xFF;
+  buffer[(*len) + 1] = value & 0xFF;
+  (*len) += sizeof(uint16_t);
 
   return buffer;
 }
