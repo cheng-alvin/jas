97b085fe9ead247b1c847638878637b8b907ddd5
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-15 21:56:16 +1100

    Update aaa.h

--- src/aaa.h
+++ src/aaa.h
@@ -5,7 +5,7 @@
  * @since v0.0.1
  *
  * This function writes the `AAA` instruction to the specified buffer
- * inside the first parameter. The `AAA` instruction is used to adjust
+ * inside the hints. The `AAA` instruction is used to adjust
  * the result of a previous `DAA` instruction.
  *
  * @note Please see the intel manual for more information on the instruction.
