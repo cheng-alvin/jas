59133f3f0be9c4b2051d327e1ee2787e41047d0d
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-17 08:10:20 +1100

    Update error.c
    Fixes `return discards qualifiers from pointer target type` from compiler

--- src/error.c
+++ src/error.c
@@ -1,6 +1,6 @@
 #include "error.h"
 
 char *jasErrorno(jasErrorCode_t c) {
-  const char *codes[] = {"JAS_NO_ERROR"}; // TODO Add more error codes here!
+  char *codes[] = {"JAS_NO_ERROR"}; // TODO Add more error codes here!
   return codes[c];
 }
