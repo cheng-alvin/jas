b2bf3882c2821a9a94ceefea88ac05df5f023cfa
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-23 14:37:46 +1100

    Deleted unit test stuff

--- engine/test/main.c
+++
@@ -1,8 +0,0 @@
-#include "color.h"
-#include <stdio.h>
-
-int main() {
-  printf(RED "Hello world" RESET);
-
-  return 0;
-}
--- util/test/main.c
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-#include <stdio.h>
-
-int main() {
-
-  return 0;
-}
