56f77c513ee2d8bcfccc14386119fdf0c109bdb7
cheng-alvin <88267875+cheng-alvin@users.noreply.github.com>
2023-11-28 15:06:27 +1100

    Added undefined guards

--- libjas/error.c
+++ libjas/error.c
@@ -1,6 +1,8 @@
 #include "error.h"
 
 char *jasErrorno(jasErrorCode_t c) {
-  char *codes[] = {"JAS_NO_ERROR", "JAS_ERROR_UNDEFINED_POINTER"}; // TODO Add more error codes here!
+  char *codes[] = {"JAS_NO_ERROR", "JAS_ERROR_UNDEFINED_POINTER"}; // TODO Add more error codes
+  if (c > sizeof(c) * sizeof(char *))
+    return "JAS_NO_ERROR_CODE";
   return codes[c];
 }
