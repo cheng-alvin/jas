99e3a3fc378ba30ba8e13df545f522c377c55eee
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-28 18:03:21 +1100

    Added documentation for `jasToInstructionEnum`

--- libjas/include/instruction.h
+++ libjas/include/instruction.h
@@ -3,4 +3,19 @@ typedef enum {
   AAD,
 } instruction_t;
 
+/**
+ * @author cheng-alvin
+ * @since v0.0.1
+ *
+ * Converts a string instruction into an `instruction_t` enum.
+ * useful for converting a string instruction into an enum and
+ * allowing for using `switch` statement multiple times in the
+ * code. (Basically allows for a faster speed).
+ *
+ * @see `instruction_t`
+ *
+ * @param instruction The instruction to convert to an enum.
+ * @returns The enum of the instruction.
+ */
+
 instruction_t jasToInstructionEnum(char *instruction);
