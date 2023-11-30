270d424fe9a8766021250f805eee50c0fecc38ef
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-30 18:51:23 +1100

    Implemented write functions

---
+++ libjas/write.c
@@ -0,0 +1,16 @@
+#include "write.h"
+#include <stdint.h>
+
+uint8_t *write8(uint8_t *buffer, uint8_t value, int len) {
+  len++;
+  buffer = realloc(len * sizeof(uint8_t));
+  buffer[len - 1] = value;
+  return buffer;
+}
+
+uint8_t *write16(uint8_t *buffer, uint16_t value, int len) {
+  buffer = write8(buffer, (uint8_t)value >> 8, len);
+  len++;
+  buffer = write8(buffer, (uint8_t)value, len);
+  return buffer;
+}
