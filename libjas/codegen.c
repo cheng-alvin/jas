fdbbf803cbd9d99998e81267efc8af7e7a0f52af
cheng-alvin <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 10:07:41 +1100

    Added codegen inital files

---
+++ libjas/codegen.c
@@ -0,0 +1,8 @@
+#include "error.h"
+#include <stdint.h>
+#include "init.h"
+#include "codegen.h"
+
+jasErrorCode_t codegen(jasInstruction_t instruction, jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, uint8_t *buf, jasInstance_t instance){
+	return JAS_NO_ERROR;
+}
---
+++ libjas/include/codegen.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+typdef union {
+	uint8_t u8;
+	uint16_t u16;
+	uint32_t u32;
+	uint64_t u64;
+} jasOperand_t
+
