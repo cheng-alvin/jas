29baa84922d030d827a5cd8a62ae0a4b865da9ed
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-11-30 18:41:03 +1100

    Added new line for formatting

--- libjas/error.c
+++ libjas/error.c
@@ -2,7 +2,9 @@
 
 char *jasErrorno(jasErrorCode_t c) {
   char *codes[] = {"JAS_NO_ERROR", "JAS_ERROR_UNDEFINED_POINTER"}; // TODO Add more error codes
+
   if (c > sizeof(c) * sizeof(char *))
     return "JAS_NO_ERROR_CODE";
+
   return codes[c];
 }
