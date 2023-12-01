e870835e8ac1fb35e01b736e3481103d35f35727
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 22:48:20 +1100

    Added some placeholder instruction generation units

---
+++ libjas/aaa.c
@@ -0,0 +1,10 @@
+#include "codegen.h"
+#include "error.h"
+#include "init.h"
+#include <stdint.h>
+
+jasErrorCode_t aaa(jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, uint8_t *buffer, jasInstance_t instance) {
+  // TODO Implement this!
+
+  return JAS_NO_ERROR;
+}
