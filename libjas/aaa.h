28aaf124efc2f057a97c5d4e572e1382f6bb345a
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 23:09:03 +1100

    added todo comment

--- libjas/aaa.h
+++ libjas/aaa.h
@@ -3,4 +3,6 @@
 #include "init.h"
 #include <stdint.h>
 
+// TODO Add more docs
+
 jasErrorCode_t aaa(jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, jasInstance_t *instance);
