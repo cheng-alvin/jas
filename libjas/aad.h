83a616b5c65f0771c03d3010222c67d935ce154e
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 23:17:46 +1100

    Added aad instruction

---
+++ libjas/aad.h
@@ -0,0 +1,10 @@
+#include "codegen.h"
+#include "error.h"
+#include "init.h"
+#include "null.h"
+#include "write.h"
+#include <stdint.h>
+
+// TODO Add more docs
+
+jasErrorCode_t aad(jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, jasInstance_t *instance);
