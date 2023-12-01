c23f6922168bf7bea59e4c03b9c9bdd4356523ea
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 22:48:31 +1100

    Added types

--- libjas/include/codegen.h
+++ libjas/include/codegen.h
@@ -1,8 +1,12 @@
 #include <stdint.h>
 
+// TODO Add documentation.
+
 typedef union {
   uint8_t u8;
   uint16_t u16;
   uint32_t u32;
   uint64_t u64;
 } jasOperand_t;
+
+typedef jasErrorCode_t (*jasInstructionEncoderPointerArray_t[])(jasOperand_t, jasOperand_t, jasOperand_t, jasOperand_t, uint8_t *, jasInstance_t);
