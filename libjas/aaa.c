c3fb56df1895765848a1d34fbffee0d53cab3005
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-01 22:52:50 +1100

    Allows pointer to be realloced

--- libjas/aaa.c
+++ libjas/aaa.c
@@ -1,10 +1,10 @@
 #include "codegen.h"
 #include "error.h"
 #include "init.h"
+#include "write.h"
 #include <stdint.h>
 
-jasErrorCode_t aaa(jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, uint8_t *buffer, jasInstance_t instance) {
-  // TODO Implement this!
-
+jasErrorCode_t aaa(jasOperand_t op1, jasOperand_t op2, jasOperand_t op3, jasOperand_t op4, uint8_t **buffer, jasInstance_t instance) {
+  *buffer = write8(buffer, 0x37, sizeof(buffer) / sizeof(uint8_t));
   return JAS_NO_ERROR;
 }
--- libjas/codegen.c
+++ libjas/codegen.c
@@ -15,7 +15,5 @@ jasErrorCode_t codegen(jasInstruction_t instruction, jasOperand_t op1, jasOperan
   if (instruction > sizeof(jasInstructionEncoder) / sizeof(&aaa))
     return JAS_ERROR_INVALID_INSTRUCTION;
 
-  jasInstructionEncoder[instruction](op1, op2, op3, op4, buf, instance);
-
-  return JAS_NO_ERROR;
+  return jasInstructionEncoder[instruction](op1, op2, op3, op4, &buf, instance);
 }
