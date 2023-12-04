81facbe336a60519c46ed638d0d6f90820ba0e85
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-04 22:00:52 +1100

    Created size functions
    Size functions are used for finding the sizes of operands and registers. Helps with code generation

---
+++ libjas/include/size.h
@@ -0,0 +1,41 @@
+#ifndef JAS_SIZE_H
+#define JAS_SIZE_H
+
+#include "error.h"
+#include "null.h"
+#include "operand.h"
+#include <stdint.h>
+
+/**
+ * @author cheng-alvin
+ * @since v0.0.1
+ *
+ * Finds the size of an operand. Just simply searches the operand
+ * union for information and checking if it's `NULL`.
+ *
+ * @note Not used for finding the size of a register, Please use
+ * `jasRegisterSize` instead.
+ *
+ * @param x The operand to find the size of.
+ * @returns The size of the operand.
+ */
+
+int8_t jasOperandSize(jasOperand_t x);
+
+/**
+ * @author cheng-alvin
+ * @since v0.0.1
+ *
+ * Finds the size of a register operand. Just simply searches the operand
+ * union for information and checking if it's `NULL`.
+ *
+ * @note Not used for finding the size of an immediate. Please use
+ * `jasOperandSize` instead.
+ *
+ * @param x The operand to find the size of.
+ * @returns The size of the register operand.
+ */
+
+int8_t jasRegisterSize(jasOperand_t x);
+
+#endif
