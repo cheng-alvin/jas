ed95f8a70f5667d25d7d8061cca6269fd5d50500
cheng-alvin <eventide1029@gmail.com>
2023-12-06 08:53:02 +1100

    Fixed size issue

--- libjas/error.c
+++ libjas/error.c
@@ -3,7 +3,7 @@
 char *jasErrorno(jasErrorCode_t c) {
   char *codes[] = {"JAS_NO_ERROR", "JAS_UNDEFINED_POINTER", "JAS_INVALID_INSTRUCTION", "JAS_NON_LONG_MODE_INSTRUCTION", "JAS_OPERAND_ERROR", "JAS_MODE_ERROR"}; // TODO Add more error codes
 
-  if (c > sizeof(c) * sizeof(char *))
+  if (c > sizeof(codes) / sizeof(char *))
     return "JAS_NO_ERROR_CODE";
 
   return codes[c];
