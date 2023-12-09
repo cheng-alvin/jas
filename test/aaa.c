045e1ebedb63b6502a1abea5a0d8b602aaa22dab
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-09 22:48:13 +1100

    Wrote new test-case for `aaa.c`

---
+++ test/aaa.c
@@ -0,0 +1,21 @@
+#include "aaa.h"
+#include "init.h"
+#include "labrador.h"
+#include "null.h"
+
+TEST(aaa) {
+  jasInstance_t instance;
+  uint8_t *buffer = malloc(1); // TODO Affected by adding allocation for `buffer`
+  const jasErrorCode_t initExitStatus = (JAS_MODE_32, &instance, buffer, NULL, NULL, NULL, false);
+  SHOULD_EQUAL(initExitStatus, JAS_NO_ERROR)
+
+  jasErrorCode_t aaaExitStatus = aaa(JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &instance);
+  SHOULD_EQUAL(aaaExitStatus, JAS_NO_ERROR)
+  SHOULD_EQUAL(buffer[0], 0x37)
+
+  instance.mode = JAS_MODE_64;
+  aaaExitStatus = aaa(JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &instance);
+  SHOULD_EQUAL(aaaExitStatus, JAS_NON_LONG_MODE_INSTRUCTION)
+
+  PASS
+}
