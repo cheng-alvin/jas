716280b925fa46bf880e08539a5db797cb6206c4
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-10 20:11:35 +1100

    Fixed test case
    🤦‍♂️ Forgotten function name

--- test/aaa.c
+++ test/aaa.c
@@ -2,11 +2,12 @@
 #include "init.h"
 #include "labrador.h"
 #include "null.h"
+#include <stdlib.h>
 
 TEST(aaa) {
   jasInstance_t instance;
   uint8_t *buffer = malloc(1); // TODO Affected by adding allocation for `buffer`
-  const jasErrorCode_t initExitStatus = (JAS_MODE_32, &instance, buffer, NULL, NULL, NULL, false);
+  const jasErrorCode_t initExitStatus = jasInitNew(JAS_MODE_32, &instance, buffer, NULL, NULL, NULL, false);
   SHOULD_EQUAL(initExitStatus, JAS_NO_ERROR)
 
   jasErrorCode_t aaaExitStatus = aaa(JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &instance);
