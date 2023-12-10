4cff4d56c2727ede8feb3cd01bede66675c39674
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-10 19:55:29 +1100

    Update write.c
    🤦‍♂️ Forgotten to supply a target pointer to `realloc`

--- libjas/write.c
+++ libjas/write.c
@@ -1,9 +1,10 @@
 #include "write.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 uint8_t *write8(uint8_t *buffer, uint8_t value, int len) {
   len++;
-  buffer = realloc(len * sizeof(uint8_t));
+  buffer = realloc(buffer, len * sizeof(uint8_t));
   buffer[len - 1] = value;
   return buffer;
 }
