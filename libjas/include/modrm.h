ea996594959b5808af7bf77370a150401dbfe4ba
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-12 20:24:20 +1100

    WIP: Created modrm functions

---
+++ libjas/include/modrm.h
@@ -0,0 +1,10 @@
+typedef enum {
+  JAS_MODRM_REGISTER_OPERAND = 0b00000000,
+  JAS_MODRM_ADDRESS = 0b01000000,
+  JAS_MODRM_32_DISPLACEMENT = 0b10000000,
+  JAS_MODRM_REGISTER = 0b11000000,
+} jasModRMMode_t;
+
+// TODO Docs:
+
+uint8_t jasGenerateModRM(jasModRMMode_t mod, uint8_t reg, uint8_t rm);
