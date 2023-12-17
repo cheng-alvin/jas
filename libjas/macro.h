08bc876a07d2b870de9ed1006fef5663530026bf
GitHub Action <action@github.com>
2023-12-17 23:10:53 +0000

    Format code with Clang

--- libjas/macro.h
+++ libjas/macro.h
@@ -1,11 +1,11 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#define WRITE(x)                                                       \
+#define WRITE(x)                                                    \
   instance.buffer = write8(instance.buffer, x, instance.bufferLen); \
   instance.bufferLen++;
 
-#define WRITE_LE_16(x)                                                  \
+#define WRITE_LE_16(x)                                               \
   instance.buffer = write16(instance.buffer, x, instance.bufferLen); \
   instance.bufferLen++;
 
