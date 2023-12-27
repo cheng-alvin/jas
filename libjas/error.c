7568ce8c1da805e556d21b676a0da26a2fcdac8e
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-28 10:24:47 +1100

    Registed error code

--- libjas/error.c
+++ libjas/error.c
@@ -1,7 +1,7 @@
 #include "error.h"
 
 char *jasErrorno(jasErrorCode_t c) {
-  char *codes[] = {"JAS_NO_ERROR", "JAS_UNDEFINED_POINTER", "JAS_INVALID_INSTRUCTION", "JAS_NON_LONG_MODE_INSTRUCTION", "JAS_OPERAND_ERROR", "JAS_MODE_ERROR"}; // TODO Add more error codes
+  char *codes[] = {"JAS_NO_ERROR", "JAS_UNDEFINED_POINTER", "JAS_INVALID_INSTRUCTION", "JAS_NON_LONG_MODE_INSTRUCTION", "JAS_OPERAND_ERROR", "JAS_MODE_ERROR", "JAS_INVALID_HIGH_REGISTER"}; // TODO Add more error codes
 
   if (c > sizeof(codes) / sizeof(char *))
     return "JAS_NO_ERROR_CODE";
