195eca2da289115b9d9e11afe8fefab36ca86f80
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-30 16:10:12 +1100

    Fulfilled TODO of adding documentation

--- libjas/include/aaa.h
+++ libjas/include/aaa.h
@@ -3,6 +3,26 @@
 #include "init.h"
 #include <stdint.h>
 
-// TODO Add more docs
+/**
+ * @author cheng-alvin
+ * @since v0.0.1
+ *
+ * Function for Generating the code for the `aaa` instruction, normally
+ * used internally. Please call this function through the `codegen()` function.
+ *
+ * @param op1 The first operand to be encoded.
+ * @param op2 The second operand to be encoded.
+ * @param op3 The third operand to be encoded.
+ * @param op4 The fourth operand to be encoded.
+ * @param instance The instance of the assembler unit.
+ *
+ * @returns The error code if the encoding was successful.
+ *
+ * @note All parameters are not required. Please see the intel manual for more
+ * information on the `aaa` instruction.
+ *
+ * @note This instruction is NOT allowed to be  encoded in 64-bit of long mode.
+ * (Which throws an error if the mode is set to 64-bit)
+ */
 
 jasErrorCode_t aaa(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance);
