64d4ffdd24b4711171849ad80f724a489cd433d9
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-12-31 10:07:10 +1100

    Update macro.h

--- libjas/macro.h
+++ libjas/macro.h
@@ -12,20 +12,15 @@
   instance->buffer = write16(instance->buffer, x, instance->bufferLen); \
   instance->bufferLen += 2;
 
-#define WRITE32(x)            \
-  WRITE16((x >> 16) & 0xffff) \
-  WRITE16(x & 0xffff)
+// // #define WRITE32(x)            \
+// //   WRITE16((x >> 16) & 0xffff) \
+// //   WRITE16(x & 0xffff)
 
-#define WRITE64(x)            \
-  WRITE16((x >> 48) & 0xffff) \
-  WRITE16((x >> 32) & 0xffff) \
-  WRITE16((x >> 16) & 0xffff) \
-  WRITE16(x & 0xffff)
-
-#define CONDITIONAL_WRITE(x, y) \
-  if (x) {                      \
-    WRITE(y)                    \
-  }
+// // #define WRITE64(x)            \
+// //   WRITE16((x >> 48) & 0xffff) \
+// //   WRITE16((x >> 32) & 0xffff) \
+// //   WRITE16((x >> 16) & 0xffff) \
+// //   WRITE16(x & 0xffff)
 
 // Note that all of these macros are not used in the code, please uncomment if they are used. - 12/31/23
 // // #define WRITE_LE_16_IF(x, y) \
@@ -43,10 +38,10 @@
 // //     WRITE_64(y)              \
 // //  }
 
-#define BREAKABLE_CONDITIONAL_WRITE(x, y) \
-  if (x) {                                \
-    WRITE(y)                              \
-    break;                                \
+#define CONDITIONAL_WRITE(x, y) \
+  if (x) {                      \
+    WRITE(y)                    \
+    break;                      \
   }
 
 // // #define WRITE_LE_16_IF_BREAKS(x, y) \
