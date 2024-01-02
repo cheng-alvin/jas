17dabddd870cd77417a2c1581a9ca2f77a9d298f
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-01-02 22:30:46 +1100

    Renamed functions for naming scheme

--- libjas/aaa.c
+++ libjas/aaa.c
@@ -5,7 +5,7 @@
 #include "write.h"
 #include <stdint.h>
 
-jasErrorCode_t aaa(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
+jasErrorCode_t jasAAA(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
   if (instance->mode == JAS_MODE_64)
     return JAS_NON_LONG_MODE_INSTRUCTION;
 
--- libjas/aad.c
+++ libjas/aad.c
@@ -6,7 +6,7 @@
 #include "write.h"
 #include <stdint.h>
 
-jasErrorCode_t aad(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
+jasErrorCode_t jasAAD(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
   if (instance->mode == JAS_MODE_64)
     return JAS_NON_LONG_MODE_INSTRUCTION;
 
