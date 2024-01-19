622e0ea4642f745af7af67c6c3b002738011db79
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-01-19 14:15:40 +1100

    Created basic source files for the `clflush` instruction

---
+++ libjas/clflush.c
@@ -0,0 +1,32 @@
+#include "codegen.h"
+#include "error.h"
+#include "init.h"
+#include "macro.h"
+#include "modrm.h"
+#include "null.h"
+#include "rex.h"
+#include "write.h"
+#include <stdint.h>
+
+jasErrorCode_t jasCLFLUSH(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
+  WRITE(0x0F)
+  WRITE(0xAE)
+
+  switch (op1.type) {
+  case JAS_INDIRECT_8:
+    WRITE(jasGenerateModrm(JAS_MODRM_INDIRECT, 7, op1.operand.reg.indirectReg8))
+    break;
+
+  case JAS_INDIRECT_16:
+    WRITE(jasGenerateModrm(JAS_MODRM_INDIRECT, 7, op1.operand.reg.indirectReg16))
+    break;
+
+  case JAS_INDIRECT_32:
+    WRITE(jasGenerateModrm(JAS_MODRM_INDIRECT, 7, op1.operand.reg.indirectReg32))
+    break;
+
+  case JAS_INDIRECT_64:
+    WRITE(jasGenerateModrm(JAS_MODRM_INDIRECT, 7, op1.operand.reg.indirectReg64))
+    break;
+  }
+}
