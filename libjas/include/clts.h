d185b772c82b7e0ee3298810fab1c100a3129373
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-01-26 22:11:17 +1100

    Added new clts instruction

---
+++ libjas/include/clts.h
@@ -0,0 +1,8 @@
+#include "codegen.h"
+#include "error.h"
+#include "init.h"
+#include "macro.h"
+#include "write.h"
+#include <stdint.h>
+
+jasErrorCode_t jasCLTS(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance);
