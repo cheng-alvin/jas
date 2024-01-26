e3cd8f4cc54a92537c5a00da1c501df7ec8e5776
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-01-26 17:59:00 +1100

    Created `cli` instruction

---
+++ libjas/cli.c
@@ -0,0 +1,11 @@
+#include "codegen.h"
+#include "error.h"
+#include "init.h"
+#include "macro.h"
+#include "write.h"
+#include <stdint.h>
+
+jasErrorCode_t jasCLI(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
+  WRITE(0xFA)
+  return JAS_NO_ERROR;
+}
