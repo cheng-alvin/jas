4c0e96c7d1053e853ed985b74a1eb03573ac445a
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-02-09 07:37:02 +1100

    Added new test case for aaa

---
+++ test/aaa.c
@@ -0,0 +1,26 @@
+#include "codegen.h"
+#include "init.h"
+#include "labrador.h"
+#include <stdlib.h>
+
+TEST() {
+  jasInstance_t realModeInstance;
+  jasInitNew(JAS_MODE_16, &realModeInstance, JAS_NULL, JAS_NULL);
+
+  const jasErrorCode_t realModeErrorCode = jasCodegen(AAA, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &realModeInstance);
+
+  SHOULD_EQUAL(realModeErrorCode, JAS_NO_ERROR);
+  SHOULD_EQUAL(realModeInstance.buffer[0], 0x37);
+
+  free(realModeInstance.buffer);
+
+  jasInstance_t longModeInstance;
+  jasInitNew(JAS_MODE_64, &longModeInstance, JAS_NULL, JAS_NULL);
+
+  const jasErrorCode_t longModeErrorCode = jasCodegen(AAA, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &longModeInstance);
+
+  SHOULD_EQUAL(longModeErrorCode, JAS_NON_LONG_MODE_INSTRUCTION);
+  SHOULD_EQUAL(longModeInstance.bufferLen, 0);
+
+  PASS
+}
