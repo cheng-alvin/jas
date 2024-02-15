9395f81b453d79fa4fdfb6a14f8ead4287c36ec0
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-02-15 16:54:28 +1100

    Added aas test case

---
+++ test/aas.c
@@ -0,0 +1,27 @@
+#include "codegen.h"
+#include "init.h"
+#include "labrador/labrador.h"
+
+TEST() {
+  jasInstance_t realModeInstance;
+  jasInitNew(JAS_MODE_16, &realModeInstance, JAS_NULL, JAS_NULL);
+
+  const jasErrorCode_t realModeErrorCode = jasCodegen(AAS, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &realModeInstance);
+
+  SHOULD_EQUAL(realModeErrorCode, JAS_NO_ERROR);
+  SHOULD_EQUAL(realModeInstance.buffer[0], 0x3F);
+
+  free(realModeInstance.buffer);
+
+  jasInstance_t longModeInstance;
+  jasInitNew(JAS_MODE_64, &longModeInstance, JAS_NULL, JAS_NULL);
+
+  const jasErrorCode_t longModeErrorCode = jasCodegen(AAS, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, JAS_NO_OPERAND, &longModeInstance);
+
+  SHOULD_EQUAL(longModeErrorCode, JAS_NON_LONG_MODE_INSTRUCTION);
+  SHOULD_EQUAL(longModeInstance.bufferLen, 0);
+
+  free(longModeInstance.buffer);
+
+  PASS
+}
