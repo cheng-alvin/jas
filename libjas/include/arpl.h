1d940e454c0cad56ce2d72b90b18ac51d685f2e2
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-03-03 15:49:26 +1100

    Added `arpl` header file declaration

---
+++ libjas/include/arpl.h
@@ -0,0 +1,10 @@
+#include "error.c"
+#include "macro.h"
+#include "mr.h"
+#include "null.h"
+#include "operand.c"
+#include <stdint.h>
+
+// Awaiting documentation TODO
+
+jasErrorCode_t jasARPL(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance);
