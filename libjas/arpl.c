d208a8beb08b42f2c13decc0e439666166b7f5d7
Alvin cheng <eventide1029@gmail.com>
2024-03-03 15:41:31 +1100

    Added `arpl` instruction code TBC
    - Needs tests
    - Needs header files
    - Awaiting registration to enums and lists

---
+++ libjas/arpl.c
@@ -0,0 +1,19 @@
+#include "error.c"
+#include "macro.c"
+#include "mr.h"
+#include "null.h"
+#include "operand.c"
+#include <stdint.h>
+
+// TODO Added unit tests
+// TODO add to enum and lists
+//
+jasErrorCode_t jasARPL(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance) {
+  if (instance->mode == JAS_MODE_64)
+    return JAS_NON_LONG_MODE_INSTRUCTION;
+
+  WRITE(0x63)
+  jasExtendedOperandIdentityMR(op1, op2, op3, op4, instance, JAS_MODRM_REGISTER, JAS_NULL);
+
+  return JAS_NO_ERROR;
+}
