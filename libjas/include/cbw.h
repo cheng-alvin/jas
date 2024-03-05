fa43682027677e7ddff4e348e9ed41846ec76ec3
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-03-05 17:13:35 +1100

    Added header guards and docs for `cbw.h`

--- libjas/include/cbw.h
+++ libjas/include/cbw.h
@@ -1,15 +1,28 @@
-#include "codegen.h"
-#include "error.h"
+#ifndef CBW_H
+#define CBW_H
+
 #include "init.h"
-#include "macro.h"
-#include "modrm.h"
-#include "null.h"
 #include "operand.h"
-#include "rex.h"
-#include "rm.h"
-#include "write.h"
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
+
+/**
+ * Function for Generating the code for the `cbw` instruction, normally
+ * used internally. Please call this function through the `jasCodegen()` function.
+ *
+ * @param op1 The first operand to be encoded.
+ * @param op2 The second operand to be encoded.
+ * @param op3 The third operand to be encoded.
+ * @param op4 The fourth operand to be encoded.
+ * @param instance The instance of the assembler unit.
+ *
+ * @returns The error code if the encoding was successful.
+ *
+ * @note All parameters are not required. Please see the intel manual for more
+ * information on the `cbw` instruction.
+ *
+ * @note This instruction is NOT allowed to be encoded in 64-bit of long mode.
+ * (Which throws an error if the mode is set to 64-bit)
+ */
 
 jasErrorCode_t jasCBW(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance);
+
+#endif
