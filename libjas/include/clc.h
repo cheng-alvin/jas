4ce7a187bec406bc5395c4338edefa8b8e4edd41
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-03-05 17:22:11 +1100

    Refactored  and  headers

--- libjas/include/clc.h
+++ libjas/include/clc.h
@@ -1,10 +1,28 @@
-#include "codegen.h"
+#ifndef CLC_H
+#define CLC_H
+
 #include "error.h"
-#include "init.h"
-#include "macro.h"
-#include "null.h"
-#include "rex.h"
-#include "write.h"
-#include <stdint.h>
+#include "operand.h"
+
+/**
+ * Function for Generating the code for the `clc` instruction, normally
+ * used internally. Please call this function through the `jasCodegen()` function.
+ *
+ * @param op1 The first operand to be encoded.
+ * @param op2 The second operand to be encoded.
+ * @param op3 The third operand to be encoded.
+ * @param op4 The fourth operand to be encoded.
+ * @param instance The instance of the assembler unit.
+ *
+ * @returns The error code if the encoding was successful.
+ *
+ * @note All parameters are not required. Please see the intel manual for more
+ * information on the `clc` instruction.
+ *
+ * @note This instruction is NOT allowed to be encoded in 64-bit of long mode.
+ * (Which throws an error if the mode is set to 64-bit)
+ */
 
 jasErrorCode_t jasCLC(jasTaggedOperand_t op1, jasTaggedOperand_t op2, jasTaggedOperand_t op3, jasTaggedOperand_t op4, jasInstance_t *instance);
+
+#endif
