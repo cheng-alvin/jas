04de1737e08d2800855f3f1cbea23a9cb1ee1374
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-03-25 19:20:35 +1100

    Made single-lined for loops and if checks claused

--- libjas/buffer.c
+++ libjas/buffer.c
@@ -31,21 +31,18 @@
 void write(buffer_t *buf, const uint8_t *data, const size_t data_len) {
   buf->data = (uint8_t *)(buf->data == NULL ? malloc(data_len) : realloc(buf->data, buf->len + data_len));
 
-  for (size_t i = 0; i < data_len; i++) {
+  for (size_t i = 0; i < data_len; i++)
     buf->data[buf->len + i] = data[i];
-  }
 
   buf->len += data_len;
 }
 
 void remove(buffer_t *buf, const size_t elem) {
-  if (buf->len == 0) {
+  if (buf->len == 0)
     return;
-  }
 
-  for (size_t i = elem; i < buf->len - 1; i++) {
+  for (size_t i = elem; i < buf->len - 1; i++)
     buf->data[i] = buf->data[i + 1];
-  }
 
   buf->len--;
   buf->data = realloc(buf->data, buf->len);
@@ -55,7 +52,6 @@ void remove_chunk(buffer_t *buf, const size_t start, const size_t end) {
   if (buf->len == 0)
     return;
 
-  for (size_t i = start; i < end; i++) {
+  for (size_t i = start; i < end; i++)
     remove(buf, i);
-  }
 }
