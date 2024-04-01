e631bad0ee6a28d05605b5cae42148c9c591b74b
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-04-01 12:41:04 +1100

    removed previously commented macros

--- libjas/include/register.h
+++ libjas/include/register.h
@@ -26,93 +26,6 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
-// #define REG_AL 0
-// #define REG_CL 1
-// #define REG_DL 2
-// #define REG_BL 3
-// #define REG_AH 4
-// #define REG_CH 5
-// #define REG_DH 6
-// #define REG_BH 7
-
-// #define REG_AX 0
-// #define REG_CX 1
-// #define REG_DX 2
-// #define REG_BX 3
-// #define REG_SP 4
-// #define REG_BP 5
-// #define REG_SI 6
-// #define REG_DI 7
-
-// #define REG_EAX 0
-// #define REG_ECX 1
-// #define REG_EDX 2
-// #define REG_EBX 3
-// #define REG_ESP 4
-// #define REG_EBP 5
-// #define REG_ESI 6
-// #define REG_EDI 7
-
-// #define REG_RAX 0
-// #define REG_RCX 1
-// #define REG_RDX 2
-// #define REG_RBX 3
-// #define REG_RSP 4
-// #define REG_RBP 5
-// #define REG_RSI 6
-// #define REG_RDI 7
-
-// // REX.B dictated registers:
-
-// #define REG_R8B 0
-// #define REG_R9B 1
-// #define REG_R10B 2
-// #define REG_R11B 3
-// #define REG_R12B 4
-// #define REG_R13B 5
-// #define REG_R14B 6
-// #define REG_R15B 7
-
-// #define REG_R8W 0
-// #define REG_R9W 1
-// #define REG_R10W 2
-// #define REG_R11W 3
-// #define REG_R12W 4
-// #define REG_R13W 5
-// #define REG_R14W 6
-// #define REG_R15W 7
-
-// #define REG_R8D 0
-// #define REG_R9D 1
-// #define REG_R10D 2
-// #define REG_R11D 3
-// #define REG_R12D 4
-// #define REG_R13D 5
-// #define REG_R14D 6
-// #define REG_R15D 7
-
-// #define REG_R8 0
-// #define REG_R9 1
-// #define REG_R10 2
-// #define REG_R11 3
-// #define REG_R12 4
-// #define REG_R13 5
-// #define REG_R14 6
-// #define REG_R15 7
-
-// #define REG_SPL 4
-// #define REG_BPL 5
-// #define REG_SIL 6
-// #define REG_DIL 7
-
-// // Segment registers: (But not dictated by REX.B)
-// #define REG_ES 0
-// #define REG_CS 1
-// #define REG_SS 2
-// #define REG_DS 3
-// #define REG_FS 4
-// #define REG_GS 5
-
 enum registers {
   REG_AL,
   REG_CL,
@@ -198,7 +111,5 @@ enum registers {
   REG_FS,
   REG_GS,
 };
-
-// TODO(Also random side note I've realized) We need a way to ensure that the REX prefixes and registers work as intended with the B prefix and that high order registers do not conflict with REX prefixes!!
 
 #endif
