46d9acefc4a1ec2423eb5384f6b435b7d5cfc08f
Alvin cheng <eventide1029@gmail.com>
2024-04-23 13:21:28 +1000

    Changed integer type to `int8_t` to allow negative numbers

--- libjas/mr.c
+++ libjas/mr.c
@@ -30,12 +30,12 @@
 #include <stdint.h>
 
 // Static definitions
-static uint8_t operand_mode(const operand_t *op_arr);
+static int8_t operand_mode(const operand_t *op_arr);
 
 void mr(const operand_t *op_arr, const buffer_t *buf, __attribute__((__unused)) const instr_encode_table_t *instr_ref) {
   const uint8_t *reg = op_arr[0].data;
   const uint8_t *rm = op_arr[1].data;
-  const uint8_t mode = operand_mode(op_arr);
+  const int8_t mode = operand_mode(op_arr);
 
   if (mode == -1)
     return;
@@ -43,7 +43,7 @@ void mr(const operand_t *op_arr, const buffer_t *buf, __attribute__((__unused)) 
   buf_write(buf, mode << 6 | *reg << 3 | *rm, 1);
 }
 
-static uint8_t operand_mode(const operand_t *op_arr) {
+static int8_t operand_mode(const operand_t *op_arr) {
   if (OP_R(op_arr[0].type))
     return OP_MODRM_REG;
   else if (OP_M(op_arr[0].type))
