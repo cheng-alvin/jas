35be74aa3c0b9816ed8fdf06293bec8646bb9635
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-05-11 22:18:57 +1000

    Cleaned up code

--- libjas/mr.c
+++ libjas/mr.c
@@ -29,28 +29,21 @@
 #include "operand.h"
 #include <stdint.h>
 
-// Static definitions
-static int8_t operand_mode(const operand_t *op_arr);
-
 void mr(const operand_t *op_arr, const buffer_t *buf, __attribute__((__unused)) const instr_encode_table_t *instr_ref) {
-  const uint8_t *reg = op_arr[0].data;
-  const uint8_t *rm = op_arr[1].data;
-  const int8_t mode = operand_mode(op_arr);
+  uint8_t mode;
 
-  if (mode == -1)
-    return;
-
-  buf_write(buf, mode << 6 | *reg << 3 | *rm, 1);
-}
-
-static int8_t operand_mode(const operand_t *op_arr) {
   if (op_r(op_arr[0].type))
-    return OP_MODRM_REG;
+    mode = OP_MODRM_REG;
   else if (op_m(op_arr[0].type))
-    return OP_MODRM_INDIRECT;
+    mode = OP_MODRM_INDIRECT;
 
   else {
     err("Operand identity mismatch. (hint: displacements are not supported yet)");
-    return -1;
+    return;
   }
+
+  const uint8_t *reg = op_arr[0].data;
+  const uint8_t *rm = op_arr[1].data;
+
+  buf_write(buf, mode << 6 | *reg << 3 | *rm, 1);
 }
