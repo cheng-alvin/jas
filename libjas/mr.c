000ddd3b72fdbf452903217e768e13fcaef1316c
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-05-20 22:08:23 +1000

    WIP: add prefixes in instruction identity

--- libjas/mr.c
+++ libjas/mr.c
@@ -26,24 +26,33 @@
 #include "mr.h"
 #include "buffer.h"
 #include "error.h"
+#include "mode.h"
 #include "operand.h"
 #include <stdint.h>
 
-void mr(const operand_t *op_arr, const buffer_t *buf, __attribute__((__unused)) const instr_encode_table_t *instr_ref) {
+void mr(const operand_t *op_arr, const buffer_t *buf, const instr_encode_table_t *instr_ref, const enum modes mode) {
+  if (op_sizeof(op_arr[0].type) != op_sizeof(op_arr[1].type)) {
+    err("Invalid operand sizes.");
+    return;
+  }
+
+  // TODO strange 1st parameter behavior
+  op_set_prefix(buf, op_arr[0].type);
+
   const uint8_t *reg = op_arr[0].data;
   const uint8_t *rm = op_arr[1].data;
 
-  uint8_t mode;
+  uint8_t mr_mode;
 
   if (op_r(op_arr[0].type))
-    mode = OP_MODRM_REG;
+    mr_mode = OP_MODRM_REG;
   else if (op_m(op_arr[0].type))
-    mode = OP_MODRM_INDIRECT;
+    mr_mode = OP_MODRM_INDIRECT;
 
   else {
     err("Operand identity mismatch. (hint: displacements are not supported yet)");
     return;
   }
 
-  buf_write(buf, mode << 6 | *reg << 3 | *rm, 1);
+  buf_write(buf, mr_mode << 6 | *reg << 3 | *rm, 1);
 }
