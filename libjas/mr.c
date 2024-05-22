cf5456e68c5805b236b2ea168320da2a0dd9e67d
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-05-22 17:16:14 +1000

    Formatted code using clang

--- libjas/mr.c
+++ libjas/mr.c
@@ -32,9 +32,8 @@
 #include "rex.h"
 #include <stdint.h>
 
-// TODO Refactor code 
+// TODO Refactor code
 // It's f---ing ugly
-
 
 void mr(const operand_t *op_arr, const buffer_t *buf, const instr_encode_table_t *instr_ref, const enum modes mode) {
 
