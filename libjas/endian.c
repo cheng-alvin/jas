ce4beb4de3e5a2611602f1226122a32e533deb50
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-06-26 21:04:07 +1000

    Added endian functions with documentation

--- libjas/endian.c
+++ libjas/endian.c
@@ -1,10 +1,39 @@
+/**
+ * MIT License
+ * Copyright (c) 2023-2024 Alvin Cheng (eventide1029@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @see `LICENSE`
+ */
+
+#include "endian.h"
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-void endian(uint8_t *data, size_t data_size) {
-  uint8_t result[data_size];
+// TODO Double check functionality for endian
+uint8_t *endian(uint8_t *data, size_t data_size) {
+  uint8_t *result = (uint8_t *)malloc(data_size * sizeof(uint8_t));
+
   for (size_t i = data_size; i > 0; i--)
-    result[data_size - i] = data[i];
+    result[data_size - i] = data[i - 1];
 
-  data = result;
+  return result;
 }
