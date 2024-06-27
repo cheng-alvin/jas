3b79a36bde68eddc30480bbcd072eab0b3898733
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-06-27 23:02:57 +1000

    Added new line

--- libjas/buffer.c
+++ libjas/buffer.c
@@ -36,6 +36,7 @@ void buf_write(buffer_t *buf, const uint8_t *data, const size_t data_len) {
 
   buf->len += data_len;
 }
+
 void buf_write_byte(buffer_t *buf, const uint8_t data) {
   buf_write(buf, (uint8_t[]){data}, 1);
 }
