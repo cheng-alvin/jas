e8469fe8d1b0f07510132e2fb9aa8733cc6cada1
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-07-06 16:29:54 +1000

    Updated instruction.c
    Applied preprocessor function for the instruction.c file's existing
    entries and added the `lea` instruction's entry.

--- libjas/instruction.c
+++ libjas/instruction.c
@@ -41,6 +41,7 @@ instr_encode_table_t mov[] = {
         .byte_instr_opcode = {0x88},
         .should_fallback_support = false,
         .opcode_size = 1,
+        .pre = NULL,
     },
     {
         .ident = OP_RM,
@@ -50,6 +51,7 @@ instr_encode_table_t mov[] = {
         .byte_instr_opcode = {0x8A},
         .should_fallback_support = false,
         .opcode_size = 1,
+        .pre = NULL,
     },
     {
         .ident = OP_OI,
@@ -59,22 +61,47 @@ instr_encode_table_t mov[] = {
         .byte_instr_opcode = {0xB0},
         .should_fallback_support = false,
         .opcode_size = 1,
+        .pre = NULL,
+
     },
     {
         .ident = OP_MI,
-        .opcode_ext = 0,
+        .opcode_ext = 0b10000000,
         .opcode = {0xC7},
         .support = NULL,
         .byte_instr_opcode = {0xC6},
         .should_fallback_support = false,
         .opcode_size = 1,
+        .pre = NULL,
     },
 
     NULL // Terminator
 
 };
 
-instr_encode_table_t *instr_table[] = {mov};
+// Experimental
+static void pre_lea(operand_t *op_arr, buffer_t *buf, instr_encode_table_t *instr_ref, enum modes mode) {
+  if (op_m(op_arr[0].type) || op_r(op_arr[1].type))
+    err("Invalid operands type for LEA instruction.");
+
+  if (op_sizeof(op_arr[0].type) == 8)
+    err("Byte operands cannot be used with the LEA instruction.");
+}
+
+instr_encode_table_t lea[] = {
+    {
+        .ident = OP_RM,
+        .opcode_ext = NULL,
+        .opcode = {0x8D},
+        .support = NULL,
+        .byte_instr_opcode = {0x8D},
+        .should_fallback_support = false,
+        .opcode_size = 1,
+        .pre = &pre_lea,
+    },
+};
+
+instr_encode_table_t *instr_table[] = {mov, lea};
 
 instr_encoder_t instr_encode_func(enum op_ident input) {
   instr_encoder_t lookup[] = {&mr, &rm, &oi, &mi};
