c640c74689e89729d3557560b83ae903003e350b
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-07-06 21:52:23 +1000

    Added the `i` instruction encoder identity

---
+++ libjas/i.c
@@ -0,0 +1,48 @@
+/**
+ * MIT License
+ * Copyright (c) 2023 Alvin Cheng (eventide1029@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @see `LICENSE`
+ */
+
+#include "i.h"
+#include "buffer.h"
+#include "error.h"
+#include "mode.h"
+#include "operand.h"
+#include "register.h"
+#include <stdint.h>
+
+void i(operand_t *op_arr, buffer_t *buf, instr_encode_table_t *instr_ref, enum modes mode) {
+  if (op_sizeof(op_arr[0].type) != op_sizeof(op_arr[1].type)) {
+    err("Invalid operand sizes.");
+    return;
+  }
+
+  if (reg_lookup_val(op_arr[0].data) != 0) {
+    err("Instruction identity must use an \"A\" register!");
+    return;
+  }
+
+  const uint8_t imm_size = op_sizeof(op_arr[1].type) / 8;
+  uint8_t *imm = endian(op_arr[1].data, imm_size);
+  buf_write(buf, imm, imm_size);
+}
