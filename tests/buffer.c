54b51668d79d83250c7f249edd44d6c541bfb118
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-07-22 23:06:11 +1000

    Implemented more test cases

--- tests/buffer.c
+++ tests/buffer.c
@@ -8,7 +8,37 @@ Test(buffer, write) {
 
   buf_write(&buf, data, sizeof(data));
 
-  cr_assert(buf.data[0] == 0xBE);
+  cr_assert(buf.data[0] = 0xBE);
   cr_assert(buf.data[1] == 0xEF);
   cr_assert(buf.len == sizeof(data));
+}
+
+Test(buffer, write_byte) {
+  buffer_t buf = BUF_NULL;
+
+  buf_write_byte(&buf, 0xBE);
+
+  cr_assert(buf.data[0] = 0xBE);
+  cr_assert(buf.len == 1);
+}
+
+Test(buffer, remove) {
+  buffer_t buf = BUF_NULL;
+
+  buf_write_byte(&buf, 0xBE);
+  buf_write_byte(&buf, 0xEF);
+
+  buf_remove(&buf, 0);
+
+  cr_assert(buf.data[0] == 0xEF);
+  cr_assert(buf.len == 1);
+}
+
+Test(buffer, element_exists) {
+  buffer_t buf = BUF_NULL;
+
+  buf_write_byte(&buf, 0xBE);
+
+  cr_assert(buf_element_exists(&buf, 0xBE));
+  cr_assert(!buf_element_exists(&buf, 0xEF));
 }
