d506557948e85a7cb55a0e1ba2ca16422974bc08
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-08-06 17:01:12 +1000

    Moved run script away

---
+++ tests/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in $(ls); do
+    if [[ -x "$file" && $file != "test.sh" ]]; then
+        ./"$file"
+
+        if [[ $? != 0 ]]; then
+            kill $$
+        fi
+    fi
+done
