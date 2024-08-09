5a41f823b488df9884d22e3b415fd962e877d46a
Alvin cheng <eventide1029@gmail.com>
2024-08-09 11:35:38 +1000

    WIP: forgotten to commit in prev

--- tests/i.c
+++ tests/i.c
@@ -1,11 +1,18 @@
+#include "operand.h"
+#include "register.h"
 #include "test.h"
+#include <stdint.h>
+#include "i.h"
 
 Test(i, i) {
+  const operand_t al = op_construct_operand(OP_R32, 0, &(enum registers){REG_EAX});
+  const operand_t imm = op_construct_operand(OP_IMM32, 0, &(uint32_t){0xbeefface});
+	
 }
 
 int main(void) {
-  TestSuite(i)
-      RunTest(i, i)
+  TestSuite(i);
+  RunTest(i, i);
 
-          return 0;
+  return 0;
 }
