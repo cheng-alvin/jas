c60ac5f46f2421128449fa6a079bdc70a40b36af
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-08-11 18:29:53 +1000

    Updated error.c

--- tests/error.c
+++ tests/error.c
@@ -5,13 +5,6 @@ void test_callback(const char *msg) {
   assert_str_eq(msg, "msg", "Error callback function was not called correctly.");
 }
 
-Test(error, add_callback) {
-  err_add_callback(test_callback);
-
-  assert_not_null(err_callback);
-  assert(err_callback == test_callback);
-}
-
 Test(error, err) {
   err_add_callback(test_callback);
   err("msg");
@@ -20,7 +13,6 @@ Test(error, err) {
 int main(void) {
   TestSuite(error);
 
-  RunTest(error, add_callback);
   RunTest(error, err);
 
   return 0;
