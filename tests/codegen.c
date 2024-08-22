58b751684342bfa2320f7ec77c212245fab386e9
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-08-22 17:44:25 +1000

    Updated codegen.c unit test

--- tests/codegen.c
+++ tests/codegen.c
@@ -3,15 +3,17 @@
 #include "register.h"
 #include "test.h"
 
-#define INIT(x, op1, op2, op3, op4)                  \
-  {(instruction_t){                                  \
-      .instr = x,                                    \
-      .operands = (operand_t[]){op1, op2, op3, op4}, \
-  }};
+#define INIT(x, op1, op2, op3, op4)                \
+  (instruction_t) {                                \
+    .instr = x,                                    \
+    .operands = (operand_t[]){op1, op2, op3, op4}, \
+  }
 
 Test(codegen, mov) {
   const instruction_t instr[] =
-      INIT(INSTR_MOV, m8, r8, OP_NONE, OP_NONE);
+      {
+          INIT(INSTR_MOV, m8, r8, OP_NONE, OP_NONE),
+      };
 
   err_add_callback(&test_error_handler);
   buffer_t buf = codegen(MODE_LONG, instr, sizeof(instr));
