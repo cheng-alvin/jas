7f5897943865a1cb51719593c4bcf8a5f05cd938
Alvin cheng <eventide1029@gmail.com>
2024-08-23 11:14:29 +1000

    Added test cases

--- tests/codegen.c
+++ tests/codegen.c
@@ -12,7 +12,11 @@
 Test(codegen, mov) {
   const instruction_t instr[] =
       {
-          INIT(INSTR_MOV, m8, r8, OP_NONE, OP_NONE),
+          INIT(INSTR_MOV, m64, r64, OP_NONE, OP_NONE),
+          INIT(INSTR_MOV, r64, m64, OP_NONE, OP_NONE),
+          INIT(INSTR_MOV, r64, r64, OP_NONE, OP_NONE),
+          INIT(INSTR_MOV, r64, imm64, OP_NONE, OP_NONE),
+          INIT(INSTR_MOV, m64, imm64, OP_NONE, OP_NONE),
       };
 
   err_add_callback(&test_error_handler);
@@ -20,9 +24,13 @@ Test(codegen, mov) {
 
   assert_not_null(buf.data);
 
-  assert(buf.data[0] == 0x88);
-  assert(buf.data[1] == 0x40);
-  assert(buf.data[2] == 0xFF);
+  assert(buf.data[0] == 0x48);
+  assert(buf.data[1] == 0x89);
+  assert(buf.data[2] == 0x80);
+  assert(buf.data[3] == 0xff);
+  assert(buf.data[4] == 0x00);
+  assert(buf.data[5] == 0x00);
+  assert(buf.data[6] == 0x00);
 
   free(buf.data);
 }
