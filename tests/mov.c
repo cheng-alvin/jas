9dbb27137d847c99bd572e9615319072f24bd801
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-08-26 18:34:04 +1000

    Added test conditions and passed!!!!!! :D (Finally after 1000 commits)

--- tests/mov.c
+++ tests/mov.c
@@ -25,6 +25,7 @@ Test(mov, mr) {
 
   assert_not_null(buf.data);
 
+  // TODO write a function to check buffer using va_args
   assert(buf.data[0] == 0x48);
   assert(buf.data[1] == 0x89);
   assert(buf.data[2] == 0x80);
@@ -32,6 +33,30 @@ Test(mov, mr) {
   assert(buf.data[4] == 0x00);
   assert(buf.data[5] == 0x00);
   assert(buf.data[6] == 0x00);
+
+  assert(buf.data[7] == 0x67);
+  assert(buf.data[8] == 0x89);
+  assert(buf.data[9] == 0x80);
+  assert(buf.data[10] == 0xff);
+  assert(buf.data[11] == 0x00);
+  assert(buf.data[12] == 0x00);
+  assert(buf.data[13] == 0x00);
+
+  assert(buf.data[14] == 0x66);
+  assert(buf.data[15] == 0x67);
+  assert(buf.data[16] == 0x89);
+  assert(buf.data[17] == 0x80);
+  assert(buf.data[18] == 0xff);
+  assert(buf.data[19] == 0x00);
+  assert(buf.data[20] == 0x00);
+  assert(buf.data[21] == 0x00);
+
+  assert(buf.data[22] == 0x88);
+  assert(buf.data[23] == 0x80);
+  assert(buf.data[24] == 0xff);
+  assert(buf.data[25] == 0x00);
+  assert(buf.data[26] == 0x00);
+  assert(buf.data[27] == 0x00);
 
   free(buf.data);
 }
