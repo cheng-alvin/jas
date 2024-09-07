65b7fba101f913a4543ae63199adb9e3cf729f00
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-09-07 14:02:21 +1000

    Fixed wrong type

--- libjas/oi.c
+++ libjas/oi.c
@@ -33,7 +33,7 @@
 #include <stdlib.h>
 
 void oi(operand_t *op_arr, buffer_t *buf, instr_encode_table_t *instr_ref, enum modes mode) {
-  const enum registers reg = reg_lookup_val(op_arr[0].data);
+  const uint8_t reg = reg_lookup_val(op_arr[0].data);
 
   const buffer_t prefixes = op_write_prefix(op_arr, mode);
   buf_write(buf, prefixes.data, prefixes.len);
