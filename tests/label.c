2672ca35c1019bfd617fb06cf8af8872adbd8c68
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-10-02 19:51:10 +1000

    Update label.c

--- tests/label.c
+++ tests/label.c
@@ -2,22 +2,21 @@
 #include "test.h"
 
 Test(label, create) {
-  label_create("label", false, false, 0xFFFF);
-  label_t *labels = label_dump_all();
+  label_create("label", false, false, 0xFFFF, NULL);
 
   assert_str_eq(
-      labels[0].name, "label",
+      label_table[0].name, "label",
       "Label name is incorrect.");
 
-  assert(labels[0].exported == false);
-  assert(labels[0].ext == false);
-  assert(labels[0].address == 0xFFFF);
+  assert(label_table[0].exported == false);
+  assert(label_table[0].ext == false);
+  assert(label_table[0].address == 0xFFFF);
 
   label_destroy_all();
 }
 
 Test(label, lookup) {
-  label_create("label", false, false, 0xFFFF);
+  label_create("label", false, false, 0xFFFF, NULL);
   label_t *label = label_lookup("label");
 
   assert_str_eq(
