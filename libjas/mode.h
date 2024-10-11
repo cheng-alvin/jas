97c9baa0007f88b71f28959db94d965160f9e694
Alvin Cheng <eventide1029@gmail.com>
2024-10-11 19:10:12 +1100

    Update mode.h

--- libjas/mode.h
+++ libjas/mode.h
@@ -29,11 +29,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/**
- * Enums representing the operating modes of
- * the x86 processor as defined by the Intel
- * manual.
- */
 enum modes {
   MODE_REAL,
   MODE_PROTECTED,
