26f159b7e3e1e5d973159f673c4255eab5b0baf7
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-10-26 15:50:42 +1100

    Partially complete generation of the section headers

--- libjas/codegen.c
+++ libjas/codegen.c
@@ -45,8 +45,28 @@ buffer_t codegen(enum modes mode, instruction_t *instr_arr, size_t arr_size, enu
     return BUF_NULL;
   }
 
-  buffer_t header = exe_header(0x40, 3, 0);
-  // buffer_t code_sect = exe_sect_header(0, )
+  char shstrtab[] = "\0.shstrtab\0.strtab\0.symtab\0.text\0";
+  char strtab[] = "\0";
+
+  buffer_t header = exe_header(0x40, 4, 0);
+
+  /**
+   * @note
+   * The file offset of the section header table as seen with the expressions
+   * such as `5 * 0x40 + sizeof(shstrtab)` etc is shown as `5 * 0x40` because
+   * the ELF header is 64 bytes long and the section header table is 64 bytes
+   * EACH, given that there are 4 sections, the total size of the section header
+   * table is 256 bytes, hence the `5 * 0x40` expression.
+   *
+   * Also, the data offset must also be taken into account when calculating the
+   * file offset of the section header table. as we need some space for the data
+   * itself for the section.
+   */
+
+  buffer_t shstrtab_sect_head = exe_sect_header(1, 0x03, 0, 5 * 0x40, sizeof(shstrtab));
+  buffer_t strtab_sect_head = exe_sect_header(11, 0x03, 0, 5 * 0x40 + sizeof(shstrtab), sizeof(strtab));
+  buffer_t symtab_sect_head = exe_sect_header(19, 0x02, 0, 5 * 0x40 + sizeof(shstrtab) + sizeof(strtab), 0); /* TODO generate symbol table */
+  buffer_t text_sect_head = exe_sect_header(27, 0x01, 0, 5 * 0x40 + sizeof(shstrtab) + sizeof(strtab) /* + Symbol table size */, out.len);
 
   return out;
 }
