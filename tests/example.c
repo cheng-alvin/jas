0969879b4e1f262ca33870cd0dc2c6fd9e380e62
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-11-25 19:34:35 +1100

    Removed some instructions for testing

--- tests/example.c
+++ tests/example.c
@@ -10,84 +10,24 @@ Test(example, example) {
   err_add_callback(error_callback);
 
   instruction_t instr[] = (instruction_t[]){
-      //   // jmp _start
-      //   (instruction_t){
-      //       .instr = INSTR_JMP,
-      //       .operands = (operand_t[]){
-      //           (operand_t){
-      //               .type = OP_REL32,
-      //               .label = "_start",
-      //           },
-      //           OP_NONE,
-      //           OP_NONE,
-      //           OP_NONE,
-      //       },
-      //   },
-      //   // global _start:
-      //   (instruction_t){
-      //       .instr = INSTR_DIR_GLOBAL_LABEL,
-      //       .operands = (operand_t[]){
-      //           (operand_t){.type = OP_MISC, .data = "_start"},
-      //           OP_NONE,
-      //           OP_NONE,
-      //           OP_NONE,
-      //       },
-      //   },
-
-      //   // mov rax, 60
-      //   (instruction_t){
-      //       .instr = INSTR_MOV,
-      //       .operands = (operand_t[]){
-      //           (operand_t){
-      //               .type = OP_R64,
-      //               .data = &(enum registers){REG_RAX},
-      //           },
-      //           (operand_t){
-      //               .type = OP_IMM64,
-      //               .data = &(uint64_t){60},
-      //           },
-      //           OP_NONE,
-      //           OP_NONE,
-      //       },
-      //   },
-      //   // xor rdi, rdi
-      //   (instruction_t){
-      //       .instr = INSTR_XOR,
-      //       .operands = (operand_t[]){
-      //           (operand_t){
-      //               .type = OP_R64,
-      //               .data = &(enum registers){REG_RDI},
-      //           },
-      //           (operand_t){
-      //               .type = OP_R64,
-      //               .data = &(enum registers){REG_RDI},
-      //           },
-      //           OP_NONE,
-      //           OP_NONE,
-      //       },
-      //   },
-      //   // syscall
-      //   (instruction_t){
-      //       .instr = INSTR_SYSCALL,
-      //       .operands = (operand_t[]){
-      //           OP_NONE,
-      //           OP_NONE,
-      //           OP_NONE,
-      //           OP_NONE,
-      //       },
-      //   },
-
-      /**
-       * square(int):
-          push    rbp
-          mov     rbp, rsp
-          mov     DWORD PTR [rbp-4], edi
-          mov     eax, DWORD PTR [rbp-4]
-          imul    eax, eax
-          pop     rbp
-          ret
-       */
-
+      // mov double(int), rax
+      (instruction_t){
+          .instr = INSTR_MOV,
+          .operands = (operand_t[]){
+              (operand_t){
+                  .type = OP_M64,
+                  .data = &(enum registers){REG_RBP},
+                  .offset = 0,
+                  //   .label = "double(int)",
+              },
+              (operand_t){
+                  .type = OP_R64,
+                  .data = &(enum registers){REG_RAX},
+              },
+              OP_NONE,
+              OP_NONE,
+          },
+      },
       // square(int):
       (instruction_t){
           .instr = INSTR_DIR_GLOBAL_LABEL,
@@ -98,109 +38,7 @@ Test(example, example) {
               OP_NONE,
           },
       },
-      // push rbp
-      (instruction_t){
-          .instr = INSTR_PUSH,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_R64,
-                  .data = &(enum registers){REG_RBP},
-              },
-              OP_NONE,
-              OP_NONE,
-              OP_NONE,
-          },
-      },
-      // mov rbp, rsp
-      (instruction_t){
-          .instr = INSTR_MOV,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_R64,
-                  .data = &(enum registers){REG_RBP},
-              },
-              (operand_t){
-                  .type = OP_R64,
-                  .data = &(enum registers){REG_RSP},
-              },
-              OP_NONE,
-              OP_NONE,
-          },
-      },
-      // mov DWORD PTR [rbp-4], edi
-      (instruction_t){
-          .instr = INSTR_MOV,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_M32,
-                  .offset = -4,
-                  .data = &(enum registers){REG_EBP},
-              },
-              (operand_t){
-                  .type = OP_R32,
-                  .data = &(enum registers){REG_EDI},
-              },
-              OP_NONE,
-              OP_NONE,
-          },
-      },
 
-      // mov eax, DWORD PTR [rbp-4]
-      (instruction_t){
-          .instr = INSTR_MOV,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_R32,
-                  .data = &(enum registers){REG_EAX},
-              },
-              (operand_t){
-                  .type = OP_M32,
-                  .offset = -4,
-                  .data = &(enum registers){REG_EBP},
-              },
-              OP_NONE,
-              OP_NONE,
-          },
-      },
-      // add eax, eax
-      (instruction_t){
-          .instr = INSTR_ADD,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_R32,
-                  .data = &(enum registers){REG_EAX},
-              },
-              (operand_t){
-                  .type = OP_R32,
-                  .data = &(enum registers){REG_EAX},
-              },
-              OP_NONE,
-              OP_NONE,
-          },
-      },
-      // pop rbp
-      (instruction_t){
-          .instr = INSTR_POP,
-          .operands = (operand_t[]){
-              (operand_t){
-                  .type = OP_R64,
-                  .data = &(enum registers){REG_RBP},
-              },
-              OP_NONE,
-              OP_NONE,
-              OP_NONE,
-          },
-      },
-      // ret
-      (instruction_t){
-          .instr = INSTR_RET,
-          .operands = (operand_t[]){
-              OP_NONE,
-              OP_NONE,
-              OP_NONE,
-              OP_NONE,
-          },
-      },
   };
 
   buffer_t buf = codegen(MODE_LONG, instr, sizeof(instr), CODEGEN_ELF);
