d80c4569e2ae19b33a53c7db16066abc0e279811
Alvin Cheng <eventide1029@gmail.com>
2024-12-17 10:53:52 +1100

    Removed `inline` attribute to the function signature (#12)
    For some reason the `assemble_instr` funcitonc cannot be called
    using the `inline` keyword aimed at reducing production code size,
    and produces a linker error during link-time.

--- libjas/codegen.c
+++ libjas/codegen.c
@@ -208,6 +208,6 @@ static buffer_t assemble(enum modes mode, instruction_t *instr_arr, size_t arr_s
   return buf;
 }
 
-inline buffer_t assemble_instr(enum modes mode, instruction_t instr) {
+buffer_t assemble_instr(enum modes mode, instruction_t instr) {
   return codegen(mode, &instr, sizeof(instruction_t), CODEGEN_RAW);
 }
