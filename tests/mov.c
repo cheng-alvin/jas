d0de9bdc83722fb4048bbfc326c0e81ff9f8d28e
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2024-12-17 12:11:43 +1100

    Finished filling out the `mov` test
    This commit describes the changes that has completed the develop-
    ment of the `mov` test which tests the `mov` instruction, (*clap*
    *clap* wow!)
    
    This test works by leveraging the power from the standard C pre-
    processor, this test first defines the set of expected values
    in hex, then feeds it into a macro that runs the `assemble_instr`
    function and compares the bytes returned.

--- tests/mov.c
+++ tests/mov.c
@@ -1,24 +1,42 @@
 #include "jas.h"
 #include "test.h"
 
-Test(mov, mr) {
-  err_add_callback(test_error_handler);
+#define mr_bytes {0x48, 0x89, 0x80, 0xff, 0x00, 0x00, 0x00};
+#define rm_bytes {0x48, 0x8B, 0x80, 0xff, 0x00, 0x00, 0x00};
+#define oi_bytes {0x48, 0xB8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-  const instruction_t instr = {
-      .instr = INSTR_MOV,
-      .operands = (operand_t[]){m64, r64, OP_NONE, OP_NONE},
-  };
+#define mi_bytes \
+  { 0x48, 0xC7, 0x80, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF }
 
-  const buffer_t buf = assemble_instr(MODE_LONG, instr);
+// TODO: Encapsulate this into a global macro for all tests
+#define MOV_TEST(name, op1, op2, expected_bytes)                                        \
+  Test(mov, name) {                                                                     \
+    err_add_callback(test_error_handler);                                               \
+                                                                                        \
+    const operand_t operands[] = {op1, op2, OP_NONE, OP_NONE};                          \
+    const instruction_t instr = {                                                       \
+        .instr = INSTR_MOV,                                                             \
+        .operands = &(operand_t[]){operands[0], operands[1], operands[2], operands[3]}, \
+    };                                                                                  \
+                                                                                        \
+    const unsigned char expected[] = expected_bytes;                                    \
+    const buffer_t buf = assemble_instr(MODE_LONG, instr);                              \
+                                                                                        \
+    assert_eq_buf_arr(buf, expected, sizeof(expected));                                 \
+    free(buf.data);                                                                     \
+  }
 
-  unsigned char expected[] = {0x48, 0x89, 0x80, 0xff, 0x00, 0x00, 0x00};
-  assert_eq_buf_arr(buf, expected, sizeof(expected));
-  free(buf.data);
-}
+MOV_TEST(mr, m64, r64, mr_bytes);
+MOV_TEST(rm, r64, m64, rm_bytes);
+MOV_TEST(oi, r64, imm64, oi_bytes);
+MOV_TEST(mi, m64, imm64, oi_bytes);
 
 int main(void) {
   TestSuite(mov);
   RunTest(mov, mr);
+  RunTest(mov, rm);
+  RunTest(mov, oi);
+  RunTest(mov, mi);
 
   return 0;
 }
