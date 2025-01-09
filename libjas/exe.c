cheng-alvin/jas

commit e02bf0d3d624497b6d36c3c7e1e01c11fdc04197

Added pointer for tracking offset changes (#39)
This pull has introduced a pointer argument to the `exe_sect_header`
function to allow the function itself to modify the size value *out
side* of the function to help the caller (Which in this case is nor-
mally the `codegen` function) keep track of the total offset and size
of the headers before the sector of the start of the contents of the
ELF file. (Where the code, secttion table and everything else goes)

libjas/exe.c
@@ -96,7 +96,9 @@ buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_
 #define QWORD_PAD \
   &(uint64_t) { 0 }
 
-buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t off, uint64_t sect_sz) {
+buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t *off, uint64_t sect_sz) {
+  *off += sect_sz;
+
   buffer_t ret = BUF_NULL;
   buf_write(&ret, (uint8_t *)&str_offset, 4); // String table name offset
   buf_write(&ret, (uint8_t *)&type, 4);       // Section type

libjas/codegen.c
@@ -108,10 +108,10 @@ buffer_t codegen(enum modes mode, instruction_t *instr_arr, size_t arr_size, enu
    * file offset of the section header table. as we need some space for the data
    * itself for the section.
    */
-  const int base = 6 * 0x40;
+  size_t base = 6 * 0x40;
 
   char shstrtab[] = "\0.shstrtab\0.strtab\0.symtab\0.text\0";
-  buffer_t shstrtab_sect_head = exe_sect_header(1, 0x03, 0, base, sizeof(shstrtab));
+  buffer_t shstrtab_sect_head = exe_sect_header(1, 0x03, 0, &base, sizeof(shstrtab));
 
   buf_write_byte(&strtab, 0);
   buf_write(&symtab, pad, 0x18);
@@ -137,10 +137,9 @@ buffer_t codegen(enum modes mode, instruction_t *instr_arr, size_t arr_size, enu
     free(ent.data);
   }
 
-  buffer_t strtab_sect_head = exe_sect_header(11, 0x03, 0x2, base + sizeof(shstrtab), strtab.len);
-  buffer_t symtab_sect_head = exe_sect_header(19, 0x02, 0x2, base + sizeof(shstrtab) + strtab.len, symtab.len);
-
-  buffer_t text_sect_head = exe_sect_header(27, 0x01, 0x7, base + sizeof(shstrtab) + strtab.len + symtab.len, code.len);
+  buffer_t strtab_sect_head = exe_sect_header(11, 0x03, 0x2, &base, strtab.len);
+  buffer_t symtab_sect_head = exe_sect_header(19, 0x02, 0x2, &base, symtab.len);
+  buffer_t text_sect_head = exe_sect_header(27, 0x01, 0x7, &base, code.len);
 
   // Write and clean everything 🧹🧹
 

libjas/include/exe.h
@@ -48,7 +48,7 @@ buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_
  * @param str_offset The offset of the string table
  * @param type The type of the section
  * @param flags The flags of the section
- * @param off The offset of the section
+ * @param off The pointer to the offset of the section
  * @param sect_sz The size of the section
  *
  * @return The buffer containing the section header
@@ -57,8 +57,11 @@ buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_
  * tree as well as the POSIX ELF standard.
  *
  * @see https://github.com/torvalds/linux/blob/master/include/uapi/linux/elf.h
+ *
+ * @note The `off` pointer is used to keep track of the offset/size of the section
+ * headers and helps the caller to keep track of the section headers.
  */
-buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t off, uint64_t sect_sz);
+buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t *off, uint64_t sect_sz);
 
 /**
  * Function for generating a symbol table entry in the ELF object file.

commit 65894ed0fad10ee1acf2e56e9599a844926c260b

Fixed incorrect offset values.
After the switch to updating the ELF offset using pointers to allow
the offset value to be updated automatically within the function.
Some other functionality has broke since the changes in #39 and as
outlined above. First of all, the outstanding issue causing issues
after the change is the "pre mature" modification of the offset
before it was used.

In particular, the modification line:
  > *off += sect_sz;

Was placed **before** the line where *it was used*:
  > buf_write(&ret, (uint8_t *)off, 8);

This means, the value is incorrectly referenced, however, this is
not the primary issue *(I think)*. The primary issue I think is
the fact that I am referencing the pointer to the `off` pointer.

Previously, the offset was written as `&off` which is the **address**
of `off`, instead of the value.

libjas/exe.c
@@ -97,8 +97,6 @@ buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_
   &(uint64_t) { 0 }
 
 buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t *off, uint64_t sect_sz) {
-  *off += sect_sz;
-
   buffer_t ret = BUF_NULL;
   buf_write(&ret, (uint8_t *)&str_offset, 4); // String table name offset
   buf_write(&ret, (uint8_t *)&type, 4);       // Section type
@@ -106,8 +104,9 @@ buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uin
 
   buf_write(&ret, QWORD_PAD, 8); // Section address
 
-  buf_write(&ret, (uint8_t *)&off, 8);     // Section file offset
+  buf_write(&ret, (uint8_t *)off, 8);      // Section file offset
   buf_write(&ret, (uint8_t *)&sect_sz, 8); // Section size
+  *off += sect_sz;
 
   int int_pad = 0;
 