028b15774db11dcc0dc573a22fb05bf9b49d4b6a
Alvin Cheng <eventide1029@gmail.com>
2025-01-27 14:39:25 +1100

    Added example

---
+++ tests/example.c
@@ -0,0 +1,16 @@
+/* Extracted and modified from `README.md` */
+#include <jas.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  instruction_t *instr = instr_gen(INSTR_MOV, 2, r32(REG_EAX), imm32(0x12345678));
+  buffer_t buf = assemble_instr(MODE_LONG, instr);
+
+  for (int i = 0; i < buf.len; i++)
+    printf("%02X ", buf.data[i]);
+
+  free(buf.data);
+  return 0;
+}
