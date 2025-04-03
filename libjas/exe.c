538a8043e4c077c360ca9fba12bd5a85ff5bae69
Alvin Cheng <eventide1029@gmail.com>
2025-04-03 21:46:32 +1100

    Reworked ELF generation + changes to ELF generation
    This pull has done an overhaul of the ELF generation functionality
    of the Jas assembler library. Previously, all ELF object generation
    behavior has been baked into the `codegen` function, offering very
    little flexibilty for change. This also has complicated the `codegen`
    function by not only having more verbose arguments, but also cluttered
    in code. This pull has resolved most issues, re-visiting the previously
    rushed funcitonality. These changes include:
    
      - Changes to return type from the `codegen` function of `codegen_ret`
        which exposed access of the label table and label table size members
        to callers. (Something which previously was not allowed)
    
      - Cleaned up inconsistencies in the `exe.c` file, especially `buf_write`
        calls that had inconsistent endian conversions (some values used the
        jas `endian()` funciton while some were hard-coded, usually without
        much of explaination or comments), creating confusion inside the code.
    
      - Refactored out type-specific if-checks and hard-coded values: some
        section headers such as the one of the `.symtab` involved inter-dependent
        sections which were mentioned/cited in the section header. Previously,
        these were hard-coded and were of the function's descretion. Now, this
        function has now passed down by the caller through new parameters.
    
      - *And obviously, the migration of the object generation function*. This
        pull has also seen the refactoring of many things among others, steming
        out from *"reworking the ELF generation"* , which this did infact complete.
    
    This is not to be confused with later integration of the relocation tables
    and `extern` functions, this is only a small refactor/overhaul of old software,
    infavor of flexibility and maintainablity

--- libjas/exe.c
+++ libjas/exe.c
@@ -29,20 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * @brief File with functions for generating the parts of the
- * ELF executable file format. Jas is only implementing x64
- * linux ELF files, which will be implemented in the following
- * order:
- *
- * - ELF header - ✅
- * - Section headers - ✅
- * - String table - ✅
- * - Text section - ✅
- * - Symbol table - ✅
- *
- * - Relocation table
- */
+#define STD_UNIT_SIZE (uint8_t[]){0x40, 0x00}
 
 buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_str) {
   buffer_t ret = BUF_NULL;
@@ -64,80 +51,136 @@ buffer_t exe_header(size_t sect_start, uint16_t sect_count, uint16_t sect_count_
   buf_write_byte(&ret, 0x03); // Target ABI
   buf_write_byte(&ret, 0);    // ABI version
 
-  buf_write(&ret, (uint8_t[]){0, 0, 0, 0, 0, 0, 0}, 7); // Padding - 7 bytes
+  buf_write(&ret, (uint8_t *)&long_pad, 7); // Padding - Takes 7 bytes off 8
 
-  buf_write(&ret, endian((uint8_t[]){0x00, 0x01}, 2), 2); // Executable type type
-  buf_write(&ret, endian((uint8_t[]){0x00, 0x3E}, 2), 2); // Machine ISA
+  buf_write(&ret, (uint8_t[]){0x01, 0x00}, 2); // Executable type type
+  buf_write(&ret, (uint8_t[]){0x3E, 0x00}, 2); // Machine ISA
 
-  buf_write(&ret, endian((uint8_t[]){0x00, 0x00, 0x00, 0x01}, 4), 4); // ELF version
+  buf_write(&ret, (uint8_t[]){0x01, 0x00, 0x00, 0x00}, 4); // ELF version
 
   buf_write(&ret, (uint8_t *)&long_pad, 8);   // Entry point
   buf_write(&ret, (uint8_t *)&long_pad, 8);   // Program header table offset
   buf_write(&ret, (uint8_t *)&sect_start, 8); // Section header table offset
 
-  buf_write(&ret, (uint8_t *)&int_pad, 4);                // Flags - Not used
-  buf_write(&ret, endian((uint8_t[]){0x00, 0x40}, 2), 2); // ELF header size
+  buf_write(&ret, (uint8_t *)&int_pad, 4); // Flags - Not used
+  buf_write(&ret, STD_UNIT_SIZE, 2);       // ELF header size
 
   /**
    * @note The program header table is not used in this implementation of the ELF
    * file format, hence the program header table is only used when a program is
    * standalone and not a shared object or a relocatable file.
    */
-
   buf_write(&ret, (uint8_t *)&int_pad, 4); // Program header table entry size and count - Combined not used
 
-  buf_write(&ret, (uint8_t[]){0x40, 0x00}, 2);    // Section header table entry size
+  buf_write(&ret, STD_UNIT_SIZE, 2);              // Section header table entry size
   buf_write(&ret, (uint8_t *)&sect_count, 2);     // Section header table count
   buf_write(&ret, (uint8_t *)&sect_count_str, 2); // Section header table string table index
 
   return ret;
 }
 
-#define QWORD_PAD \
-  &(uint64_t){0}
-
-buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t *off, uint64_t sect_sz, size_t label_size) {
+buffer_t exe_sect_header(uint32_t str_offset, uint32_t type, uint64_t flags, uint64_t *off, uint64_t sect_sz, size_t info, uint32_t label_sect, uint64_t ent_size) {
+  const uint64_t long_pad = 0;
   buffer_t ret = BUF_NULL;
+
   buf_write(&ret, (uint8_t *)&str_offset, 4); // String table name offset
   buf_write(&ret, (uint8_t *)&type, 4);       // Section type
   buf_write(&ret, (uint8_t *)&flags, 8);      // Section flags
 
-  buf_write(&ret, QWORD_PAD, 8); // Section address
+  buf_write(&ret, (uint8_t *)&long_pad, 8); // Section address
 
   buf_write(&ret, (uint8_t *)off, 8);      // Section file offset
   buf_write(&ret, (uint8_t *)&sect_sz, 8); // Section size
   *off += sect_sz;
 
-  int int_pad = 0;
-
-  if (type == 0x02) {
-    buf_write(&ret, &(uint32_t){2}, 4);              // Section link
-    buf_write(&ret, &(uint32_t){label_size + 1}, 4); // Section info
-    buf_write(&ret, QWORD_PAD, 8);                   // Section address alignment
-    buf_write(&ret, &(uint64_t){0x18}, 8);           // Section entry size
-
-    return ret;
-  }
-
-  buf_write(&ret, (uint8_t *)&int_pad, 4); // Section link
-  buf_write(&ret, (uint8_t *)&int_pad, 4); // Section info
-  buf_write(&ret, QWORD_PAD, 8);           // Section address alignment
-  buf_write(&ret, QWORD_PAD, 8);           // Section entry size
+  // Maybe padded:
+  buf_write(&ret, label_sect, 4);            // Section link
+  buf_write(&ret, &(uint32_t){info + 1}, 4); // Section info
+  buf_write(&ret, (uint8_t *)&long_pad, 8);  // Section address alignment
+  buf_write(&ret, ent_size, 8);              // Section entry size
 
   return ret;
 }
 
 buffer_t exe_sym_ent(char *name, uint64_t sym_val, uint16_t sect_idx, buffer_t *strtab, uint8_t info) {
   buffer_t symtab = BUF_NULL;
+  uint64_t long_pad = 0;
 
   buf_write(&symtab, (uint32_t *)&strtab->len, 4); // Name offset
   buf_write_byte(&symtab, info);                   // Info
   buf_write_byte(&symtab, 0);                      // Other
   buf_write(&symtab, &(uint16_t){sect_idx}, 2);    // Section index
   buf_write(&symtab, &sym_val, 8);                 // Value
-  buf_write(&symtab, QWORD_PAD, 8);                // Size
+  buf_write(&symtab, (uint8_t *)&long_pad, 8);     // Size
 
   buf_write(strtab, (uint8_t *)name, strlen(name) + 1); // Add name to string table
 
   return symtab;
+}
+
+buffer_t exe_generate(struct codegen_ret ret) {
+  uint8_t *p_padding = calloc(1, 0x40);
+  buffer_t result_buffer = BUF_NULL;
+
+  /// @note header size of 0x40
+  size_t addr = 0x40;
+
+  // Data has been HARD-CODED rather than messing around with
+  // OOP and structs, we only need these for now, right? Add more.
+  const char shstrtab[] = "\0.shstrtab\0.strtab\0.symtab\0.text\0";
+  buffer_t shstrtab_head = exe_sect_header(1, 3, 2, &addr, sizeof(shstrtab), 0, 0, 0);
+
+  buffer_t strtab = BUF_NULL;
+  buffer_t symtab = BUF_NULL;
+
+  for (size_t i = 0; i < ret.label_table_size; i++) {
+    // Truncating 24 bytes off the 64-byte allocated buffer using `calloc`.
+    buf_write(&symtab, p_padding, 24);
+
+    const char *name = ret.label_table[i].name;
+    buf_write(&strtab, name, strlen(ret.label_table[i].name) + 1);
+
+    uint8_t binding = 0;
+    if (ret.label_table[i].exported || ret.label_table[i].ext) binding = 1;
+    const buffer_t ent =
+        exe_sym_ent(ret.label_table[i].name, ret.label_table[i].address, 4, &strtab,
+                    (((binding) << 4) + ((0) & 0xf)));
+
+    buf_concat(&symtab, 1, ent);
+    free(ent.data);
+  }
+
+  buffer_t strtab_head = exe_sect_header(11, 3, 2, &addr, strtab.len, 0, 0, 0);
+  buffer_t symtab_head = exe_sect_header(19, 2, 2, &addr, symtab.len, 0, 0, 0);
+
+  const size_t info = ret.label_table_size + 1;
+  buffer_t text_head = exe_sect_header(27, 1, 7, &addr, ret.code.len, info, 3, 24);
+
+  // Adjust the amount of sections as required.
+  const buffer_t header = exe_header(addr, 4, 3);
+  buf_write(&result_buffer, header.data, header.len);
+  buf_write(&result_buffer, p_padding, 0x40);
+
+#define FREE_ALL(...)                                                   \
+  do {                                                                  \
+    void *pointers[] = {__VA_ARGS__};                                   \
+    for (size_t i = 0; i < sizeof(pointers) / sizeof(*pointers); ++i) { \
+      free(pointers[i]);                                                \
+    }                                                                   \
+  } while (0)
+
+  buf_concat(&result_buffer, 4, shstrtab_head, strtab_head, symtab_head, text_head);
+  buf_write(&result_buffer, shstrtab, sizeof(shstrtab));
+  buf_concat(&result_buffer, 3, strtab, symtab, ret.code);
+
+  free(p_padding);
+
+  // clang-format off
+
+  FREE_ALL(
+    shstrtab_head.data, strtab.data, symtab.data, strtab_head.data, 
+    symtab_head.data, text_head.data, header.data
+  );
+
+  // clang-format on
 }
