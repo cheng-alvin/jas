b64e18662ed3f1ea291751fa18a01b65bf1efb0f
Alvin Cheng <eventide1029@gmail.com>
2025-10-17 11:07:04 +1100

    Added simple passing for compilation
    This is the intial commit for the cross compiler of the encoder table
    compiler. It should be noted that the cross compiler *does depend on*
    the `js-yaml` package for the parsing of yaml files into a usable JSON
    strcutre in the persepctive of JavaScript.

--- libjas/scripts/compile.js
+++ libjas/scripts/compile.js
@@ -1,1 +1,10 @@
-// Code for generation of the instruction.yml file
+const yaml = require("js-yaml");
+const fs = require("fs")
+
+let instructions = [];
+
+for (let i = 1; i < process.argv.length; i++){
+  const fileContents = fs.readFileSync(process.argv[i], {encoding: 'utf-8'});
+  instructions.push(yaml.load(fileContens).instructions);
+}
+
