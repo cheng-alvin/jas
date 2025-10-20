b647190edf788e4f655a320cf1ebc6eaa19665b4
Alvin Cheng <eventide1029@gmail.com>
2025-10-20 23:08:12 +1100

    Encoder table compiler implementation (#113)
    * Standarised naming for encoder identity
    
    There has been some confusing regarding the encoder struct member
    naming, Different sources of documentation has varied namings
    of the variable. Therefore, from this point onwards, the confusion
    would be eliminated by suggesting that `encoder` would be the
    **correct** naming for such value across **ALL** sources.
    
    * Completed implementation of the compilation script
    
    This commit has finalised the implementation of the compiation script
    for the encoder reference table structure. Rather than the manual
    definition of individual structures in the janky C strucutre, utilisation
    of the YAML language and a Javascript compliation step can improve
    readability, while maintain relavant information.
    
    It should be noted that despite some testing, this script is still
    unstable and should undergo more stringent testing before the complete
    reliance and migration of prevous infrastructure into the build system.

--- libjas/scripts/compile.js
+++ libjas/scripts/compile.js
@@ -1,10 +1,89 @@
-const yaml = require("js-yaml");
+const yaml = require("js-yaml"); // Ensure `js-yaml` is installed and accessible
 const fs = require("fs")
 
 let instructions = [];
+let instructionNames = [];
 
-for (let i = 1; i < process.argv.length; i++){
-  const fileContents = fs.readFileSync(process.argv[i], {encoding: 'utf-8'});
-  instructions.push(yaml.load(fileContens).instructions);
+for (let i = 2; i < process.argv.length; i++) {
+  const fileContents = fs.readFileSync(process.argv[i], {encoding : 'utf-8'});
+  const data = yaml.load(fileContents);
+
+  for (let j = 0; j < data.instructions.length; j++) {
+    instructionNames.push(Object.keys(data.instructions[j])[0]);
+    instructions.push(data.instructions[j]);
+  }
 }
 
+// TODO refactor since this is a mess
+function produceOperands(instruction) {
+  for (let i = 0; i < instruction.variants.length; i++) {
+    for (let j = 0; j < instruction.variants[i].operands.length; j++) {
+      const operand = instruction.variants[i].operands[j];
+      const match = operand.type.match(/^r\/m(8|16|32|64)$/);
+
+      if (match) {
+        let modifiedVariant = structuredClone(instruction.variants[i]);
+        instruction.variants[i].operands[j].type = `m${match[1]}`;
+
+        modifiedVariant.operands[j].type = `r${match[1]}`;
+        instruction.variants.push(modifiedVariant)
+      }
+    }
+  }
+
+  return instruction
+}
+
+instructions.forEach((instr) => {
+  produceOperands(instr);
+});
+
+function handleOperands(operands) {
+  let res = "";
+
+  for (let i = 0; i < operands.length; i++) {
+    const type = operands[i].type.toUpperCase();
+    const encoder = operands[i].encoder.toUpperCase().replace("/", "");
+
+    res += `{ .type = OP_${type}, .encoder = ENC_${encoder} }, `
+  }
+
+  return `{${res}${'{ 0 }, '.repeat(4 - operands.length)}}`
+}
+
+// clang-format off
+let output =
+    "#include <stdbool.h> \n" + "#include \"instruction.h\" \n " +
+    "#include \"operand.h\" \n " + "#include \"encoder.h\" \n\n ";
+// clang-format on
+
+output += `struct instr_encode_table instr_table[] = {`
+
+for (let k = 0; k < instructions.length; k++) {
+  const variants = instructions[k].variants;
+
+  for (let l = 0; l < variants.length; l++)
+    output += handleVariant(variants[l]);
+}
+output += "};"
+
+fs.writeFileSync("tabs.c", output);
+
+function handleVariant(variant) {
+  let res = "";
+
+  res += `.opcode = { `
+
+  variant.opcode.forEach(e => { res += `0x${e.toString(16)}, `; });
+
+  res += `${"0x00, ".repeat(3 - variant.opcode.length)}}, `
+  res += `.operand_descriptors = ${handleOperands(variant.operands)}, `
+
+  res += `.long_mode = ${variant.compatibility.long ? "true" : "false"}, `
+  res += `.leg_mode = ${variant.compatibility.legacy ? "true" : "false"}, `
+
+  res += `.opcode_size = ${variant.opcode.length}, `
+  res += `.operand_count = ${variant.operands.length},`
+
+  return `{ ${res} },`;
+}
