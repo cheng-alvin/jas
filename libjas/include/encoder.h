53403305e9b611fd25cf205f9f1546bd5c0744cc
Alvin Cheng <eventide1029@gmail.com>
2025-10-25 15:01:35 +1100

    Added note on endian-ness

--- libjas/include/encoder.h
+++ libjas/include/encoder.h
@@ -69,8 +69,8 @@ typedef struct enc_serialized_instr {
   bool has_modrm : 1;    /* ModR/M present */
   bool has_sib : 1;      /* SIB present */
 
-  uint64_t disp; /* Displacement value */
-  uint64_t imm;  /* Immediate value */
+  uint64_t disp; /* Displacement value (Little endian) */
+  uint64_t imm;  /* Immediate value  (Little endian) */
 
   /// @note A 0 size field depicts absence of the respective field.
   uint8_t disp_size : 4; /* Displacement size (0–8 bytes) */
