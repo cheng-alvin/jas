cd7fcaebaf8b3bbd0471ca158b5835d211f587a9
Alvin Cheng <eventide1029@gmail.com>
2025-11-01 22:28:40 +1100

    Enhanced REX byte support (#120)
    * Improved REX prefix support
    
    This pull improves the support for the REX prefix when used in
    combination with an instruction. Due to Intel's specification of
    needing the REX prefix to preceed the opcode, rather than the en-
    tire instruction itself. This pull now provides the new encoder
    `enc_serialized_instr` structure with a dedicated field reserved
    purely for the REX prefix.
    
    * Integrated `rex_t` and REX with `op_write_prefix`
    
    The pull has also added support for the REX prefix in line with the
    `rex_t` type alias with the existing `op_write_prefix` function.
    The pull allows a reference to the REX prefix byte to be passed,
    thus allowing for the automatic modification of the REX byte when
    used in pair with the `enc_serialized_instr` struct.

--- libjas/operand.c
+++ libjas/operand.c
@@ -47,19 +47,20 @@ uint8_t op_sizeof(enum operands input) {
   return 0;
 }
 
-void op_write_prefix(buffer_t *buf, const operand_t *op_arr, enum modes mode) {
+void op_write_prefix(buffer_t *buf, operand_t *op_arr, enum modes mode, rex_t *rex) {
   uint8_t already_written = 0;
-  uint8_t rex = REX_DEFAULT;
+  *rex = REX_DEFAULT;
 
   for (uint8_t i = 0; i < 4; i++) {
     if (op_arr[i].type == OP_NULL)
       continue;
 
     const uint8_t size = op_sizeof(op_arr[i].type);
-    uint8_t override = op_m(op_arr[i].type) ? OP_ADDR_OVERRIDE : OP_WORD_OVERRIDE;
+    const uint8_t override =
+        op_m(op_arr[i].type) ? OP_ADDR_OVERRIDE : OP_WORD_OVERRIDE;
 
     if (op_rm(op_arr[i].type))
-      rex |= reg_needs_rex(op_arr[i].data) ? REX_B : 0;
+      *rex |= reg_needs_rex(op_arr[i].data) ? REX_B : 0;
 
     switch (size) {
     case 16:
@@ -73,7 +74,8 @@ void op_write_prefix(buffer_t *buf, const operand_t *op_arr, enum modes mode) {
 
     case 32:
       if (mode == MODE_REAL) goto override_write;
-      if (override != OP_ADDR_OVERRIDE || mode == MODE_PROTECTED) break;
+      if (override != OP_ADDR_OVERRIDE) break;
+      if (mode == MODE_PROTECTED) break;
 
     override_write:
       if (already_written == override) break;
@@ -87,43 +89,40 @@ void op_write_prefix(buffer_t *buf, const operand_t *op_arr, enum modes mode) {
         break;
       }
 
-      rex |= REX_W;
+      *rex |= REX_W;
       break;
     }
   }
 
-  if (rex != REX_DEFAULT)
-    buf_write_byte(buf, rex);
+  /**
+   * @brief
+   * The 0x66 and 0x67 hex prefixes are the operand and address size
+   * overrides respectively. They are used to change the size of the operand
+   * or address in the instruction. Basically, if there's no prefix, the op-
+   * erand as well as the address size is the same as the mode's bit size.
+   * So long mode is 64 and so on.
+   *
+   * However, the override prefixes can change the size of the operand or
+   * address to 16 bits in protected mode, or 32 bits in real mode. This is
+   * useful for instructions that require a specific operand size.
+   *
+   * mode | size    | output
+   * -----------------------------
+   * real | 32      | 0x66
+   * real | 32 addr | 0x67
+   * prot | 16      | 0x66
+   * prot | 16 addr | 0x67
+   * long | 16      | 0x66
+   * long | 32 addr | 0x67
+   * long | 16 addr | 0x67 + 0x66
+   *
+   * long | 64      | REX.W
+   *
+   * Additional: Register REX prefix for REX.B
+   *
+   * @note 64-bit operands are not allowed in any modes other than long mode
+   * hence, if a 64-bit operand is present throw an error if other than
+   * long mode
+   *
+   */
 }
-
-/**
- * @brief
- * The 0x66 and 0x67 hex prefixes are the operand and address size
- * overrides respectively. They are used to change the size of the operand
- * or address in the instruction. Basically, if there's no prefix, the op-
- * erand as well as the address size is the same as the mode's bit size.
- * So long mode is 64 and so on.
- *
- * However, the override prefixes can change the size of the operand or
- * address to 16 bits in protected mode, or 32 bits in real mode. This is
- * useful for instructions that require a specific operand size.
- *
- * mode | size    | output
- * -----------------------------
- * real | 32      | 0x66
- * real | 32 addr | 0x67
- * prot | 16      | 0x66
- * prot | 16 addr | 0x67
- * long | 16      | 0x66
- * long | 32 addr | 0x67
- * long | 16 addr | 0x67 + 0x66
- *
- * long | 64      | REX.W
- *
- * Additional: Register REX prefix for REX.B
- *
- * @note 64-bit operands are not allowed in any modes other than long mode
- * hence, if a 64-bit operand is present throw an error if other than
- * long mode
- *
- */
